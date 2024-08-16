a4fd957352374e2794beb065d1707b3d9508c75e
Sergey Poluyan <spoluyan@gmail.com>
2024-08-16 11:52:16 +0200

    Support TON API header

--- src/main/kotlin/network/cere/telegram/bot/streaming/ton/TonApi.kt
+++ src/main/kotlin/network/cere/telegram/bot/streaming/ton/TonApi.kt
@@ -3,10 +3,12 @@ package network.cere.telegram.bot.streaming.ton
 import io.smallrye.common.annotation.RunOnVirtualThread
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
+import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
 import org.jboss.resteasy.reactive.RestQuery
 
 @RegisterRestClient(configKey = "ton-api")
+@ClientHeaderParam(name = "X-API-Key", value = ["\${ton.api.token}"])
 interface TonApi {
     @GET
     @Path("getTransactions")
