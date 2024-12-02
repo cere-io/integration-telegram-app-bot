3ab6c5563708a0e2aa5b99ad109889b09c5fcb9b
upalinski <ulad@cere.io>
2024-12-03 01:28:36 +0300

    creator PoC

--- src/main/kotlin/network/cere/telegram/bot/streaming/video/Video.kt
+++ src/main/kotlin/network/cere/telegram/bot/streaming/video/Video.kt
@@ -1,9 +1,11 @@
 package network.cere.telegram.bot.streaming.video
 
-import io.quarkus.hibernate.orm.panache.kotlin.PanacheCompanion
-import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntity
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheCompanionBase
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntityBase
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import kotlinx.serialization.Serializable
@@ -14,13 +16,16 @@ import network.cere.telegram.bot.streaming.channel.Channel
 @Table(name = "videos")
 @Serializable
 data class Video(
+    @Id
+    @GeneratedValue
+    var id: Long? = null,
     @Transient
     @ManyToOne(fetch = FetchType.LAZY)
     var channel: Channel? = null,
-    val url: String,
+    var url: String,
     var title: String = "",
     var description: String = "",
     var thumbnailUrl: String? = null,
-) : PanacheEntity() {
-    companion object : PanacheCompanion<Video>
+) : PanacheEntityBase {
+    companion object : PanacheCompanionBase<Video, Long>
 }
--- src/main/kotlin/network/cere/telegram/bot/streaming/video/VideosResource.kt
+++ src/main/kotlin/network/cere/telegram/bot/streaming/video/VideosResource.kt
@@ -1,8 +1,12 @@
 package network.cere.telegram.bot.streaming.video
 
 import io.smallrye.common.annotation.RunOnVirtualThread
+import jakarta.transaction.Transactional
+import jakarta.ws.rs.DELETE
 import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
 import network.cere.telegram.bot.streaming.channel.Channel
 import org.jboss.resteasy.reactive.RestHeader
 import org.jboss.resteasy.reactive.RestResponse
@@ -16,5 +20,33 @@ class VideosResource {
         if (!channel.isConfigured()) return RestResponse.notFound()
 
         return RestResponse.ok(channel.videos)
+    }
+
+    @POST
+    @RunOnVirtualThread
+    @Transactional
+    fun saveVideo(@RestHeader xTelegramChat: Long, video: Video): RestResponse<String> {
+        if (video.id == null) {
+            val channel = Channel.findById(xTelegramChat) ?: return RestResponse.notFound()
+            if (!channel.isConfigured()) return RestResponse.notFound()
+            channel.addVideo(video)
+            channel.persistAndFlush()
+        } else {
+            val entity = Video.findById(video.id!!) ?: return RestResponse.notFound()
+            entity.title = video.title
+            entity.description = video.description
+            entity.thumbnailUrl = video.thumbnailUrl
+            entity.url = video.url
+            entity.persistAndFlush()
+        }
+        return RestResponse.ok()
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @RunOnVirtualThread
+    @Transactional
+    fun deleteVideo(@RestHeader xTelegramChat: Long, @PathParam("id") id: Long): RestResponse<Boolean> {
+        return RestResponse.ok(Video.deleteById(id))
     }
 }
