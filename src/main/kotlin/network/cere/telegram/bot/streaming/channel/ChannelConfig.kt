77c064c50ceb43ada978bdbe948a7c6c4801cb2a
Jakub <jakubgil.poczta@gmail.com>
2025-01-07 15:52:56 +0100

    feat: add group handling and improve webhook command structure

--- src/main/kotlin/network/cere/telegram/bot/streaming/channel/ChannelConfig.kt
+++ src/main/kotlin/network/cere/telegram/bot/streaming/channel/ChannelConfig.kt
@@ -4,7 +4,8 @@ import jakarta.persistence.Embeddable
 
 @Embeddable
 data class ChannelConfig(
-    var botDdcAccessTokenBase58: String? = null,
-    var payoutAddress: String? = null,
-    var connectedApp: String = "cere", //TODO configure from bot apps
+        var botDdcAccessTokenBase58: String? = null,
+        var payoutAddress: String? = null,
+        var connectedApp: String = "cere", // TODO configure from bot apps
+        var connectedGroupId: Long? = null,
 )
